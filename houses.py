db1693afdb7dc380cefbd522182c799e063a74d9
Sola Olawuyi <olawuyi.sola@yahoo.com>
2021-08-18 09:37:50 +0100

    Adding comments 1

--- houses.py
+++ houses.py
@@ -33,6 +33,46 @@ for max_leaf_nodes in [5, 50, 500, 5000]:
     print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
 
 
+# DEALING WITH MISSING VALUES - DROPPING ROWS, IMPUTATION & ...
+# DROP - Fill in the lines below: drop columns in training and validation data
+    cols_missing_vals = [col for col in X_train.columns if X_train[col].isnull().any()]
+    reduced_X_train = X_train.drop(cols_missing_vals, axis=1)
+    reduced_X_valid = X_valid.drop(cols_missing_vals, axis=1)
+
+
+
+# DEALING WITH CATEGORICAL DATA - DROPPING ROWS, ORDINAL ENC & ONE HOT ENC
+# Ordinal
+    # Categorical columns in the training data
+    object_cols = [col for col in X_train.columns if X_train[col].dtype == "object"]
+
+    # Columns that can be safely ordinal encoded
+    good_label_cols = [col for col in object_cols if 
+                    set(X_valid[col]).issubset(set(X_train[col]))]
+            
+    # Problematic columns that will be dropped from the dataset
+    bad_label_cols = list(set(object_cols)-set(good_label_cols))
+            
+    print('Categorical columns that will be ordinal encoded:', good_label_cols)
+    print('\nCategorical columns that will be dropped from the dataset:', bad_label_cols)
+
+# One-Hot 
+    # Apply one-hot encoder to each column with categorical data
+    OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
+    OH_cols_train = pd.DataFrame(OH_encoder.fit_transform(X_train[object_cols]))
+    OH_cols_valid = pd.DataFrame(OH_encoder.transform(X_valid[object_cols]))
+
+    # One-hot encoding removed index; put it back
+    OH_cols_train.index = X_train.index
+    OH_cols_valid.index = X_valid.index
+
+    # Remove categorical columns (will replace with one-hot encoding)
+    num_X_train = X_train.drop(object_cols, axis=1)
+    num_X_valid = X_valid.drop(object_cols, axis=1)
+
+    # Add one-hot encoded columns to numerical features
+    OH_X_train = pd.concat([num_X_train, OH_cols_train], axis=1)
+    OH_X_valid = pd.concat([num_X_valid, OH_cols_valid], axis=1)
 
 # Opening file in VS Code from terminal
 # Type in CLI: 
