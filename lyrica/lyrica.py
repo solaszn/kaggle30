5d3667c11b25e0747e2e3f267c4fc86e1a7120ae
Sola Olawuyi <olawuyi.sola@yahoo.com>
2021-08-23 05:50:33 +0100

    #

--- lyrica/lyrica.py
+++ lyrica/lyrica.py
@@ -3,9 +3,9 @@ import lyricsgenius as lg
 import apikey
 
 genius = lg.Genius(access_token=apikey.access_token, excluded_terms=["(Remix)", "(Live)"], remove_section_headers=True)
-musician = input("Artist's Name: ")
+name = input("Artist's Name (ignore if none): ")
 song = input("What's the name of the song:")
-artist = genius.search_artist(musician, max_songs=3, sort="popularity")
-lyrica = genius.search_song(song.capitalize())
-print(artist, lyrica.lyrics)
+# singer = genius.search_artist(name, max_songs=5, sort='popularity')
+lyrica = genius.search_song(song.capitalize(), artist=name)
+print(lyrica.lyrics)
 
