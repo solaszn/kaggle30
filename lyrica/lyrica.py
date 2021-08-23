e116029c35b0fb4828a0d649f5548166d1976516
Sola Olawuyi <olawuyi.sola@yahoo.com>
2021-08-23 05:50:33 +0100

    updating lyrica

--- lyrica/lyrica.py
+++ lyrica/lyrica.py
@@ -3,9 +3,13 @@ import lyricsgenius as lg
 import apikey
 
 genius = lg.Genius(access_token=apikey.access_token, excluded_terms=["(Remix)", "(Live)"], remove_section_headers=True)
-musician = input("Artist's Name: ")
+name = input("Artist's Name (ignore if none): ")
 song = input("What's the name of the song:")
-artist = genius.search_artist(musician, max_songs=3, sort="popularity")
-lyrica = genius.search_song(song.capitalize())
-print(artist, lyrica.lyrics)
+# singer = genius.search_artist(name, max_songs=5, sort='popularity')
+lyrica = genius.search_song(song.capitalize(), artist=name)
+print(lyrica.lyrics)
 
+
+# Help Docs:
+# https://melaniewalsh.github.io/Intro-Cultural-Analytics/Data-Collection/LyricsGenius.html
+# https://medium.com/analytics-vidhya/getting-lyrics-of-songs-via-python-lyricsgenius-23e5dd5992e3
